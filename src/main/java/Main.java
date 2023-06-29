64ec833e4f7a4e80dedc94083aec6440da9192d8
Liz <liz@Zipcoders-MacBook-Pro-10.local>
2023-06-28 21:20:44 -0400

    Almost done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
@@ -8,19 +9,28 @@ public class Main {
         int number = getInputNumber();
         
         int sum = sumOfNumbers(number);
+
+        int gaussianSum = gaussianSumOfNumbers(number);
         
-        System.out.println(sum);
+        System.out.println("Traditional Method: "+sum);
+
+        System.out.println("Gaussian Method: "+gaussianSum);
+
 
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum += i;
+        }
+        return sum;
     }
     
     
-    int getInputNumber() {
+     static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -28,16 +38,20 @@ public class Main {
         int num = scan.nextInt();
 
         // Closing Scanner after the use
-        scan.close();
+         scan.close();
         return num;
     }
     
     
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+         //long startTime = System.currentTimeMillis(); //we'll figure this out later
+         int sum = (n*(n+1))/2;
+         //long endTime = System.currentTimeMillis();  //this, too...
+         return sum;
     }
+
    
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
